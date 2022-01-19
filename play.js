50c0a12fd47e021d7179c263e89a7740c3443fee
Albert Ho <albertcyi.ho@gmail.com>
2022-01-19 15:09:08 -0800

    Completed boilerplate

---
+++ play.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", msg => {
+    console.log("Connected!");
+    console.log(`Connect message: ${msg}`);
+  });
+
+  conn.on("data", msg => {
+    console.log(`Server message: ${msg}`);
+  });
+
+  // print msg sent by server
+  conn.on("end", msg => {
+    console.log("Ended!");
+    console.log(`End message: ${msg}`);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
