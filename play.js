1fd5c8cb12c31d4d0f0a7542b6360be4fa96757a
Albert Ho <albertcyi.ho@gmail.com>
2022-01-19 15:44:47 -0800

    Added functionality to listen for input and end connection with ctrl + c

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const connect = require("./client");
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    console.log("Client manually terminated!");
+    process.exit();
+  }
+};
+
 console.log("Connecting ...");
+
 connect();
+setupInput();
